342fdd8cb83c495ac55a15234dfcbd3681c09b9a
Janarthan <janarthanp@hsenidmobile.com>
2017-06-12 21:22:22 +0530

    Added blinking

--- One/index.android.js
+++ One/index.android.js
@@ -51,27 +51,65 @@
 // });
 
 
-import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+// import React, { Component } from 'react';
+// import { AppRegistry, Text, View } from 'react-native';
+//
+// class Greeting extends Component {
+//     render() {
+//         return (
+//             <Text>Hello {this.props.name}!</Text>
+//         );
+//     }
+// }
+//
+// export default class LotsOfGreetings extends Component {
+//     render() {
+//         return (
+//             <View style={{alignItems: 'center'}}>
+//               <Greeting name='Rexxar' />
+//               <Greeting name='Jaina' />
+//               <Greeting name='Valeera' />
+//             </View>
+//         );
+//     }
+// }
 
-class Greeting extends Component {
+
+import React, {Component} from 'react';
+import {AppRegistry, Text, View} from 'react-native';
+
+class Blink extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {showText: true};
+
+        //toggle the state every second
+        setInterval(() => {
+            this.setState(previousState => {
+                return {showText: !previousState.showText};
+            });
+        }, 900);
+    }
+
     render() {
+        let display = this.state.showText ? this.props.text : '';
         return (
-            <Text>Hello {this.props.name}!</Text>
+            <Text>{display}</Text>
         );
     }
+
 }
 
-export default class LotsOfGreetings extends Component {
+export default class BlinkApp extends Component {
     render() {
         return (
-            <View style={{alignItems: 'center'}}>
-              <Greeting name='Rexxar' />
-              <Greeting name='Jaina' />
-              <Greeting name='Valeera' />
+            <View>
+                <Blink text="I love to blink"/>
+                <Blink text="Blinking is great"/>
             </View>
         );
     }
 }
 
-AppRegistry.registerComponent('One', () => LotsOfGreetings);
+AppRegistry.registerComponent('One', () => BlinkApp);
